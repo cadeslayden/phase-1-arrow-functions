ab58a4a942f78dc99a07f9265d621f689dfd28e0
Cade <cslayden3@gmail.com>
2022-12-16 15:56:11 -0700

    commit tree 7a8b911be16d5bbdf424e2212cdaa4a2f2d2e225

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (parameter1, parameter2) => {
+    console.log(`dividing ${parameter1}`);
+    console.log(`dividing ${parameter2}`);
+    return parameter1/parameter2
+}
+
+const square = (parameter) => {
+    console.log(`squaring ${parameter}`);
+    return parameter * parameter
+}
+
+const add = (parameter1, parameter2) => {
+    console.log(`adding ${parameter1}`);
+    console.log(`adding ${parameter2}`);
+    return parameter1 + parameter2
+}
